3752de1142386d8475ba316436755339432990bd
Dhiraj Indurthy <dhirajindurthy@gmail.com>
2019-05-16 18:15:14 -0500

    Added codepens and sandboxes for codesamples

--- src/codesamples.js
+++ src/codesamples.js
@@ -4,11 +4,32 @@ function Codesamples(props) {
   return (
     <section className="codesamples-page">
       <iframe
-        width="500"
-        height="300"
-        scrolling="yes"
+        style={{ width: "45%" }}
+        src="https://codesandbox.io/embed/olmo6735kq?fontsize=14"
+        title="react-grid-component"
+        className="sandbox-iframe"
+        sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+      />
+      <iframe
+        src="https://codesandbox.io/embed/yvm9vxqx09?fontsize=14"
+        title="accessible-login-form"
+        style={{ width: "45%" }}
+        className="sandbox-iframe"
+        sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+      />
+      <iframe
+        src="https://codesandbox.io/embed/p5nzxk9y4j?fontsize=14"
+        title="todo-app-react"
+        style={{ width: "45%" }}
+        className="sandbox-iframe"
+        sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+      />
+      <iframe
+        height="350"
+        style={{ width: "45%" }}
+        scrolling="no"
         title="simple-angular-todo-app"
-        src="//codepen.io/dhindurthy/embed/OxYRmw/?height=265&theme-id=0&default-tab=js,result"
+        src="//codepen.io/dhindurthy/embed/OxYRmw/?height=350&theme-id=0&default-tab=js,result"
         frameborder="no"
         allowtransparency="true"
         allowfullscreen="true"
@@ -21,167 +42,171 @@ function Codesamples(props) {
         <a href="https://codepen.io/dhindurthy">@dhindurthy</a>) on{" "}
         <a href="https://codepen.io">CodePen</a>.
       </iframe>
+
       <iframe
-        width="500"
-        height="300"
-        scrolling="yes"
-        title="simple-angular-todo-app"
-        src="//codepen.io/dhindurthy/embed/OxYRmw/?height=265&theme-id=0&default-tab=js,result"
+        height="350"
+        style={{ width: "45%" }}
+        scrolling="no"
+        title="accessible-angular-form"
+        src="//codepen.io/dhindurthy/embed/Jreogj/?height=350&theme-id=0&default-tab=html,result"
         frameborder="no"
         allowtransparency="true"
         allowfullscreen="true"
       >
         See the Pen{" "}
-        <a href="https://codepen.io/dhindurthy/pen/OxYRmw/">
-          simple-angular-todo-app
+        <a href="https://codepen.io/dhindurthy/pen/Jreogj/">
+          accessible-angular-form
         </a>{" "}
         by Dhiraj Indurthy (
         <a href="https://codepen.io/dhindurthy">@dhindurthy</a>) on{" "}
         <a href="https://codepen.io">CodePen</a>.
       </iframe>
+
       <iframe
-        width="500"
-        height="300"
-        scrolling="yes"
-        title="simple-angular-todo-app"
-        src="//codepen.io/dhindurthy/embed/OxYRmw/?height=265&theme-id=0&default-tab=js,result"
+        height="350"
+        style={{ width: "45%" }}
+        scrolling="no"
+        title="sticky-header-footer"
+        src="//codepen.io/dhindurthy/embed/pWXWrg/?height=350&theme-id=0&default-tab=css,result"
         frameborder="no"
         allowtransparency="true"
         allowfullscreen="true"
       >
         See the Pen{" "}
-        <a href="https://codepen.io/dhindurthy/pen/OxYRmw/">
-          simple-angular-todo-app
+        <a href="https://codepen.io/dhindurthy/pen/pWXWrg/">
+          sticky-header-footer
         </a>{" "}
         by Dhiraj Indurthy (
         <a href="https://codepen.io/dhindurthy">@dhindurthy</a>) on{" "}
         <a href="https://codepen.io">CodePen</a>.
       </iframe>
+
       <iframe
-        width="500"
-        height="300"
-        scrolling="yes"
-        title="simple-angular-todo-app"
-        src="//codepen.io/dhindurthy/embed/OxYRmw/?height=265&theme-id=0&default-tab=js,result"
+        height="350"
+        style={{ width: "45%" }}
+        scrolling="no"
+        title="drag-and-drop-object"
+        src="//codepen.io/dhindurthy/embed/qRvyYm/?height=350&theme-id=0&default-tab=js,result"
         frameborder="no"
         allowtransparency="true"
         allowfullscreen="true"
       >
         See the Pen{" "}
-        <a href="https://codepen.io/dhindurthy/pen/OxYRmw/">
-          simple-angular-todo-app
+        <a href="https://codepen.io/dhindurthy/pen/qRvyYm/">
+          drag-and-drop-object
         </a>{" "}
         by Dhiraj Indurthy (
         <a href="https://codepen.io/dhindurthy">@dhindurthy</a>) on{" "}
         <a href="https://codepen.io">CodePen</a>.
       </iframe>
+
       <iframe
-        width="500"
-        height="300"
-        scrolling="yes"
-        title="simple-angular-todo-app"
-        src="//codepen.io/dhindurthy/embed/OxYRmw/?height=265&theme-id=0&default-tab=js,result"
+        height="350"
+        style={{ width: "45%" }}
+        scrolling="no"
+        title="mobile-modal-edit-item"
+        src="//codepen.io/dhindurthy/embed/NNGKgL/?height=350&theme-id=0&default-tab=html,result"
         frameborder="no"
         allowtransparency="true"
         allowfullscreen="true"
       >
         See the Pen{" "}
-        <a href="https://codepen.io/dhindurthy/pen/OxYRmw/">
-          simple-angular-todo-app
+        <a href="https://codepen.io/dhindurthy/pen/NNGKgL/">
+          mobile-modal-edit-item
         </a>{" "}
         by Dhiraj Indurthy (
         <a href="https://codepen.io/dhindurthy">@dhindurthy</a>) on{" "}
         <a href="https://codepen.io">CodePen</a>.
       </iframe>
+
       <iframe
-        width="500"
-        height="300"
-        scrolling="yes"
-        title="simple-angular-todo-app"
-        src="//codepen.io/dhindurthy/embed/OxYRmw/?height=265&theme-id=0&default-tab=js,result"
+        height="350"
+        style={{ width: "45%" }}
+        scrolling="no"
+        title="aria-label-labelledby"
+        src="//codepen.io/dhindurthy/embed/aLVPYQ/?height=350&theme-id=0&default-tab=html,result"
         frameborder="no"
         allowtransparency="true"
         allowfullscreen="true"
       >
         See the Pen{" "}
-        <a href="https://codepen.io/dhindurthy/pen/OxYRmw/">
-          simple-angular-todo-app
+        <a href="https://codepen.io/dhindurthy/pen/aLVPYQ/">
+          aria-label-labelledby
         </a>{" "}
         by Dhiraj Indurthy (
         <a href="https://codepen.io/dhindurthy">@dhindurthy</a>) on{" "}
         <a href="https://codepen.io">CodePen</a>.
       </iframe>
+
       <iframe
-        width="500"
-        height="300"
-        scrolling="yes"
-        title="simple-angular-todo-app"
-        src="//codepen.io/dhindurthy/embed/OxYRmw/?height=265&theme-id=0&default-tab=js,result"
+        height="350"
+        style={{ width: "45%" }}
+        scrolling="no"
+        title="mobile-modal-add-item"
+        src="//codepen.io/dhindurthy/embed/oxjvdZ/?height=350&theme-id=0&default-tab=html,result"
         frameborder="no"
         allowtransparency="true"
         allowfullscreen="true"
       >
         See the Pen{" "}
-        <a href="https://codepen.io/dhindurthy/pen/OxYRmw/">
-          simple-angular-todo-app
+        <a href="https://codepen.io/dhindurthy/pen/oxjvdZ/">
+          mobile-modal-add-item
         </a>{" "}
         by Dhiraj Indurthy (
         <a href="https://codepen.io/dhindurthy">@dhindurthy</a>) on{" "}
         <a href="https://codepen.io">CodePen</a>.
       </iframe>
+
       <iframe
-        width="500"
-        height="300"
-        scrolling="yes"
-        title="simple-angular-todo-app"
-        src="//codepen.io/dhindurthy/embed/OxYRmw/?height=265&theme-id=0&default-tab=js,result"
+        height="350"
+        style={{ width: "45%" }}
+        scrolling="no"
+        title="highcharts-bubble-drilldown"
+        src="//codepen.io/dhindurthy/embed/JJRaNq/?height=350&theme-id=0&default-tab=js,result"
         frameborder="no"
         allowtransparency="true"
         allowfullscreen="true"
       >
         See the Pen{" "}
-        <a href="https://codepen.io/dhindurthy/pen/OxYRmw/">
-          simple-angular-todo-app
+        <a href="https://codepen.io/dhindurthy/pen/JJRaNq/">
+          highcharts-bubble-drilldown
         </a>{" "}
         by Dhiraj Indurthy (
         <a href="https://codepen.io/dhindurthy">@dhindurthy</a>) on{" "}
         <a href="https://codepen.io">CodePen</a>.
       </iframe>
+
       <iframe
-        width="500"
-        height="300"
-        scrolling="yes"
-        title="simple-angular-todo-app"
-        src="//codepen.io/dhindurthy/embed/OxYRmw/?height=265&theme-id=0&default-tab=js,result"
+        height="350"
+        style={{ width: "45%" }}
+        scrolling="no"
+        title="tabs-component"
+        src="//codepen.io/dhindurthy/embed/GNMjga/?height=350&theme-id=0&default-tab=css,result"
         frameborder="no"
         allowtransparency="true"
         allowfullscreen="true"
       >
         See the Pen{" "}
-        <a href="https://codepen.io/dhindurthy/pen/OxYRmw/">
-          simple-angular-todo-app
-        </a>{" "}
+        <a href="https://codepen.io/dhindurthy/pen/GNMjga/">tabs-component</a>{" "}
         by Dhiraj Indurthy (
         <a href="https://codepen.io/dhindurthy">@dhindurthy</a>) on{" "}
         <a href="https://codepen.io">CodePen</a>.
       </iframe>
+
       <iframe
-        width="500"
-        height="300"
-        scrolling="yes"
-        title="simple-angular-todo-app"
-        src="//codepen.io/dhindurthy/embed/OxYRmw/?height=265&theme-id=0&default-tab=js,result"
+        height="350"
+        style={{ width: "45%" }}
+        scrolling="no"
+        title="accordion-web"
+        src="//codepen.io/dhindurthy/embed/VmKwOr/?height=350&theme-id=0&default-tab=css,result"
         frameborder="no"
         allowtransparency="true"
         allowfullscreen="true"
       >
         See the Pen{" "}
-        <a href="https://codepen.io/dhindurthy/pen/OxYRmw/">
-          simple-angular-todo-app
-        </a>{" "}
-        by Dhiraj Indurthy (
-        <a href="https://codepen.io/dhindurthy">@dhindurthy</a>) on{" "}
-        <a href="https://codepen.io">CodePen</a>.
+        <a href="https://codepen.io/dhindurthy/pen/VmKwOr/">accordion-web</a> by
+        Dhiraj Indurthy (<a href="https://codepen.io/dhindurthy">@dhindurthy</a>
+        ) on <a href="https://codepen.io">CodePen</a>.
       </iframe>
     </section>
   );
