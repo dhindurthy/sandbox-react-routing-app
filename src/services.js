7312597313d1fad25b05c3c36e6c2f2233440126
Dhiraj Indurthy <dhirajindurthy@gmail.com>
2019-05-23 11:04:05 -0500

    Updated width of description and added rwd media

--- src/services.js
+++ src/services.js
@@ -7,35 +7,40 @@ function Services(props) {
         Dhiraj and his Work, Services, Specialization
       </h1>
       {/* <meta name="description" content="Your meta description goes here" /> */}
-      <h2>About</h2>
-      <p tabIndex="0">
-        Dhiraj Indurthy is a self taught programmer and a Master's Degree
-        graduate in Engineering. Excelled in building, testing and maintaining
-        User Interfaces to a state where any kind of web content can be handled
-        including retail, educational, financial and small business websites.
-        Skilled with over 7 years of experience that involves building, editing
-        and optimizing websites.
-      </p>
-      <p tabIndex="0">
-        I am very passionate about Web Accessibility and treat it as a way to
-        show love for all users by providing extra help regardless of ability of
-        the user. This includes users who have visual, auditory, motor or
-        cognitive disabilities irrespective of the type or severity of
-        impairment.
-      </p>
-      <p tabIndex="0">
-        Please visit my{" "}
-        <a
-          className="inline-link"
-          href="https://linkedin.com/in/dhiraj-indurthy-43b747183"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          LinkedIn
-        </a>{" "}
-        profile for more details regarding technology stacks and institutions
-        dealt with in the past.
-      </p>
+      <section className="about-section">
+        <h2>About</h2>
+        <p tabIndex="0">
+          Dhiraj Indurthy is a self taught programmer and a Master's Degree
+          graduate in Engineering. Excelled in planning and analysis, building,
+          testing and maintaining User Interfaces to a state where any kind of
+          web content can be handled including retail, educational, financial
+          and small business websites. Skilled with over 7 years of experience
+          that involves implementing, editing and optimizing websites.
+        </p>
+        <p tabIndex="0">
+          I am very passionate about Web Accessibility and treat it as a way to
+          show love for all users by providing extra help regardless of ability
+          of the user. This includes users who have visual, auditory, motor or
+          cognitive disabilities irrespective of the type or severity of
+          impairment. I also give talks to enlighten programmers about the
+          significance of accessibility in web. Please feel free to get in touch
+          to strike up a conversation about web accessibility.
+        </p>
+        <p tabIndex="0">
+          Please visit my{" "}
+          <a
+            className="inline-link"
+            href="https://linkedin.com/in/dhiraj-indurthy-43b747183"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            LinkedIn
+          </a>{" "}
+          profile for more details regarding technology stacks and institutions
+          dealt with in the past. Please contact me if you think I would be a
+          good match to team up for your project.
+        </p>
+      </section>
       <section className="familiar-services-sections">
         <section>
           <h2>Technological Acquaintance</h2>
